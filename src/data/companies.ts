66a21926d6f87bf5f8ff9baa18d0564feba116b4
suryatejagajjela <gajjelasuryateja2021@gmail.com>
2025-12-17 19:44:10 -0500

    feat(nexus-job): update company lists to 30 contract / 70 enterprise with specific requested firms

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -1,22 +1,32 @@
 
 export const CONTRACT_COMPANIES = [
-    "Insight Global", "TekSystems", "Randstad", "Robert Half", "Kforce", "Kelly Services",
-    "ManpowerGroup", "Adecco", "Aerotek", "Collabera", "Apex Systems", "Experis",
-    "Modis", "Hays", "Michael Page", "Beacon Hill Staffing", "Mondo", "Vaco",
-    "Creative Circle", "Aquent", "Onward Search", "24 Seven", "Vitamin T",
-    "Solomon Page", "Atrium", "Motion Recruitment", "Jobspring Partners",
-    "Workbridge Associates", "CyberCoders", "Eliassen Group"
+    "Insight Global", "TekSystems", "Brooksource", "Experis", "Randstad", "Robert Half",
+    "Kforce", "Kelly Services", "ManpowerGroup", "Adecco", "Aerotek", "Collabera",
+    "Apex Systems", "Modis", "Hays", "Michael Page", "Beacon Hill Staffing", "Mondo",
+    "Vaco", "Creative Circle", "Aquent", "Onward Search", "24 Seven", "Vitamin T",
+    "Solomon Page", "Atrium", "Motion Recruitment", "Jobspring Partners", "CyberCoders",
+    "Eliassen Group"
 ];
 
 export const FULLTIME_COMPANIES = [
+    // Big Tech (MAMAA etc)
     "Amazon", "Google", "Microsoft", "Apple", "Meta", "Netflix", "Tesla", "SpaceX",
     "Nvidia", "Adobe", "Salesforce", "Oracle", "IBM", "Intel", "Cisco", "Dell",
-    "HP", "Uber", "Airbnb", "Lyft", "Spotify", "Slack", "Zoom", "Twilio", "Stripe",
-    "Square", "Coinbase", "Robinhood", "DoorDash", "Instacart", "Geico", "Capital One",
+    // FinTech & Banking
     "JPMorgan Chase", "Goldman Sachs", "Morgan Stanley", "Citi", "Bank of America",
     "Wells Fargo", "American Express", "Visa", "Mastercard", "PayPal", "Intuit",
-    "ServiceNow", "Workday", "Atlassian", "Snowflake", "Datadog", "Splunk", "Palo Alto Networks",
-    "Fortinet", "CrowdStrike", "Zscaler", "Okta", "Cloudflare", "MongoDB", "Elastic",
+    "Stripe", "Square", "Coinbase", "Robinhood", "Capital One", "Fidelity Investments",
+    "BlackRock", "Bloomberg", "SoFi",
+    // Enterprise & Cloud
+    "ServiceNow", "Workday", "Atlassian", "Snowflake", "Datadog", "Splunk",
+    "Palo Alto Networks", "Fortinet", "CrowdStrike", "Zscaler", "Okta", "Cloudflare",
+    "MongoDB", "Elastic", "Twilio", "Zoom", "Slack",
+    // Consumer & Retail
+    "Uber", "Airbnb", "Lyft", "Spotify", "DoorDash", "Instacart", "Geico",
+    "Walmart", "Target", "Home Depot", "Costco", "Nike", "Disney", "Starbucks",
+    // Telecom & Media
+    "Verizon", "T-Mobile", "AT&T", "Comcast",
+    // Gaming & Hardware
     "Unity", "Roblox", "Electronic Arts", "Activision Blizzard", "Take-Two Interactive",
-    "Sony", "Nintendo", "Samsung", "LG", "Panasonic", "Toyota", "Honda", "Ford"
-];   
+    "Sony", "Nintendo", "Samsung", "LG", "Panasonic"
+];
